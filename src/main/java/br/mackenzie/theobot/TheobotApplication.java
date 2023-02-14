6d47afedf1205e68d80bc8c862e3c6eedbcedcec
gamartins013 <gamartins013@gmail.com>
2023-02-14 20:35:34 -0300

    feat: iniciando estrutura padrao do projeto

---
+++ src/main/java/br/mackenzie/theobot/TheobotApplication.java
@@ -0,0 +1,13 @@
+package br.mackenzie.theobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mackenzie/theobot/TheobotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.theobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
