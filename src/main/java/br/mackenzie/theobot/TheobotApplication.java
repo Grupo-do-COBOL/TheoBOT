6a44fccbb249ca474e8b281a24e36c79f7aeb314
gamartins013 <gamartins013@gmail.com>
2023-02-18 23:11:07 -0300

    testando deploy

--- src/main/java/br/mackenzie/theobot/TheobotApplication.java
+++ src/main/java/br/mackenzie/theobot/TheobotApplication.java
@@ -32,7 +32,6 @@ public class TheobotApplication {
 
 
         jda.upsertCommand("ping", "comando de teste").setGuildOnly(true).queue();
-        jda.upsertCommand("teste", "outro comando de teste").setGuildOnly(true).queue();
         SpringApplication.run(TheobotApplication.class, args);
     }
 }
